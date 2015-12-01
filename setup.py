cb554572fbcff3b60497a08401668f733d54e84d
nell-byler <ebyler@astro.washington.edu>
2015-12-01 10:51:43 -0800

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cloudy-fsps',
+      version='0.1',
+      description='nebular emission for FSPS',
+      url='http://github.com/nell-byler/cloudy-fsps',
+      author='Nell Byler',
+      author_email='nell.byler@gmail.com',
+      license='BSD new',
+      packages=['cloudy-fsps'],
+      zip_safe=False)
