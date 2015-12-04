904240a77690073c5a157e0550c63f03a997a6cd
Nell Byler <ebyler@astro.washington.edu>
2015-12-04 15:37:15 -0800

    more comments and readmes

--- cloudyfsps/write_output.py
+++ cloudyfsps/write_output.py
@@ -6,6 +6,7 @@ import subprocess
 from .astrotools import air_to_vac
 from scipy.interpolate import interp1d
 import pkg_resources
+import fsps
 
 #grid: 2 files: line, cont
 
@@ -18,6 +19,8 @@ import pkg_resources
 #flux1
 #flux2
 #flux3
+sp = fsps.StellarPopulation()
+fsps_lam = sp.wavelengths
 
 class PrepOutput(object):
     def __init__(self, dir_, mod_prefix, mod_suffix, **kwargs):
@@ -27,13 +30,17 @@ class PrepOutput(object):
             self.out_pr = dir_ + mod_prefix
         else:
             self.out_pr = dir_ + mod_prefix + mod_suffix
+        # each model's final info will be in prefix00.lines, prefix00.cont
         self.line_out = self.out_pr + '.lines'
         self.cont_out = self.out_pr + '.cont'
-        self.loadModInfo()
-        self.doLineOut()
-        self.doContOut()
+        self.loadModInfo() # load each model's parameters from prefix.pars
+        self.doLineOut() # print ordered emission line wavelengths + fluxes
+        self.doContOut() # interp and print neb cont onto FSPS wavelenth arr
         return
     def loadModInfo(self, **kwargs):
+        '''
+        reads model parameters from "ZAU.pars"
+        '''
         name_keys = ['mod_num', 'logZ', 'Age', 'logU', 'logR', 'logQ', 'nH']
         data = np.genfromtxt(self.file_pr+'.pars', names=name_keys)
         self.__setattr__('modpars', data)
@@ -41,6 +48,9 @@ class PrepOutput(object):
             self.__setattr__(k, data[k])
         return
     def doLineOut(self, **kwargs):
+        '''
+        prints line fluxes to prefix00.lines file
+        '''
         f = open(self.line_out, 'w')
         self.printLineLam(f)
         for pars in self.modpars:
@@ -74,7 +84,7 @@ class PrepOutput(object):
                                                         pars['Age'],
                                                         pars['logU'])
         f.write(tstr)
-        #read in and print emission line intensities
+        #read in and print emission line intensities (Lsun/Q)
         nst = '{0:.0f}'.format(pars['mod_num'])
         filename = self.file_pr+nst+'.out_lines'
         dat = np.genfromtxt(filename, names=['lam', 'I'])
@@ -102,11 +112,14 @@ class PrepOutput(object):
         #read in and print emission line intensities
         nst = '{0:.0f}'.format(pars['mod_num'])
         filename = self.file_pr+nst+'.out_cont'
+        # read lambda, incident flux, attenuated incident, diffuse cont
         m_data = np.genfromtxt(filename, names=['lam', 'IF', 'AI', 'DC'])
+        # cloudy has vac < 3000AA and air >3000A
         x = air_to_vac(m_data['lam'])
         newx = self.fsps_lam
         fluxs_out = [m_data['IF'], m_data['AI'], m_data['DC']]
         fluxs_out = [m_data['DC']]
+        # interpolate them onto FSPS grid
         for y in fluxs_out:
             newy = interp1d(x, y)(newx)
             y_str = ' '.join(['{0: 1.4}'.format(yy) for yy in newy])
@@ -114,20 +127,20 @@ class PrepOutput(object):
         return
     def printContLam(self, f):
         '''
-        prints the wavelength array for the emission lines as the second
-        line in the output file. converts all wavelengths to vacuum.
+        prints header in first line of file, and the FSPS wavelength array
+        (for the nebular continuum) as the second line in the output file.
+        # nlam cols nmod rows logZ Age logU
+        # fsps_lam_1 fsps_lam_2 .... fsps_lam_n
         '''
-        #read in file containing wavelength info
-        linefile = pkg_resources.resource_filename(__name__, "data/fsps_lam.dat")
-        data = np.genfromtxt(linefile)
-        self.__setattr__('fsps_lam', data)
-        nlines = len(data)
+        #grab fsps wavelength info
+        self.__setattr__('fsps_lam', fsps_lam)
+        nlam = len(fsps_lam)
         nmods = np.max(self.mod_num)
         #print header to file
-        head_str = '#{0} cols {1:.0f} rows logZ Age logU'.format(nlines, nmods)
+        head_str = '#{0} cols {1:.0f} rows logZ Age logU'.format(nlam, nmods)
         f.write(head_str+'\n')
         #print lambda array
-        p_str = ' '.join(['{0:1.6e}'.format(dat) for dat in data])
+        p_str = ' '.join(['{0:1.6e}'.format(lam) for lam in fsps_lam])
         f.write(p_str+'\n')
         return
         
