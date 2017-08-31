6e8c7c14820da7c976a03de5c1b1f0de9f99b8fa
nell-byler <ebyler@astro.washington.edu>
2017-08-31 09:21:42 -0700

    pre plane

--- cloudyfsps/nebAbundTools.py
+++ cloudyfsps/nebAbundTools.py
@@ -262,7 +262,7 @@ def load_abund(set_name):
                      Ni=-5.78,
                      Cu=-7.82,
                      Zn=-7.43)
-        return adict
+    return adict
 
 def load_depl(set_name):
     if set_name == 'dopita':
@@ -277,23 +277,23 @@ def load_depl(set_name):
                      Ca = -2.52,
                      Fe = -2.0)
     elif set_name == 'newdopita':
-        ddict = dict(He =0.00,
-                     C = -0.30,
-                     N = -0.05,
-                     O = -0.07,
-                     Ne = 0.00,
-                     Na = -1.00,
-                     Mg = -1.08,
-                     Al = -1.39,
-                     Si = -0.81,
-                     S = 0.00,
-                     Cl = -1.00,
-                     Ar = 0.00,
-                     Ca = -2.52,
-                     Fe = -1.31,
-                     Ni = -2.00)
+        ddict = dict(He=0.00,
+                     C=-0.30,
+                     N=-0.05,
+                     O=-0.07,
+                     Ne=0.00,
+                     Na=-1.00,
+                     Mg=-1.08,
+                     Al=-1.39,
+                     Si=-0.81,
+                     S=0.00,
+                     Cl=-1.00,
+                     Ar=0.00,
+                     Ca=-2.52,
+                     Fe=-1.31,
+                     Ni=-2.00)
     elif set_name == 'gutkin':
-        ddict = dict(He= 0.00,
+        ddict = dict(He=0.00,
                      Li=-0.8,
                      C=-0.30,
                      O=-0.15,
